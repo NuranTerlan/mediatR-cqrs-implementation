2d6e437870583de0f4957a64425b7fa4fae0c263
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-08 17:58:13 +0400

    Create IHandlerWrapper.cs

---
+++ MediatRExample.Services/Wrappers/IHandlerWrapper.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace MediatRExample.Services.Wrappers
+{
+    public interface IHandlerWrapper<in TIn, TOut> : IRequestHandler<TIn, Response<TOut>>
+        where TIn : IRequestWrapper<TOut>
+    {
+        
+    }
+}
