181457633ca149bc18481d4287157718b6444b3b
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-08 17:57:51 +0400

    Create CreateCarCommand.cs

---
+++ MediatRExample.Services/Cars/Commands/CreateCarCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatRExample.Services.Models;
+using MediatRExample.Services.Wrappers;
+
+namespace MediatRExample.Services.Cars.Commands
+{
+    public class CreateCarCommand : IRequestWrapper<Car>
+    {
+
+    }
+}
