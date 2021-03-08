890d7e72a10065280e38767b264798b532a4b031
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-08 17:58:00 +0400

    Create GetAllCarsQuery.cs

---
+++ MediatRExample.Services/Cars/Queries/GetAllCarsQuery.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using MediatRExample.Services.Models;
+using MediatRExample.Services.Wrappers;
+
+namespace MediatRExample.Services.Cars.Queries
+{
+    public class GetAllCarsQuery : BaseRequest, IRequestWrapper<IEnumerable<Car>>
+    {
+        
+    }
+}
