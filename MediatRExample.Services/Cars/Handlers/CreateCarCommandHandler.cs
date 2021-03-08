e397b6d9149687b49609a1df1ebf51f040f6605a
NuranTerlan <nuran.terlanli@asoiu.edu.az>
2021-03-08 17:57:54 +0400

    Create CreateCarCommandHandler.cs

---
+++ MediatRExample.Services/Cars/Handlers/CreateCarCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatRExample.Services.Cars.Commands;
+using MediatRExample.Services.Models;
+using MediatRExample.Services.Wrappers;
+
+namespace MediatRExample.Services.Cars.Handlers
+{
+    public class CreateCarCommandHandler : IHandlerWrapper<CreateCarCommand, Car>
+    {
+        public Task<Response<Car>> Handle(CreateCarCommand request, CancellationToken cancellationToken)
+        {
+            // fail condition should be here
+            if (false)
+            {
+                return Task.FromResult(Response.Fail<Car>("Already exist"));
+            }
+
+            return Task.FromResult(Response.Ok<Car>(new Car { Name = "BMW" }, "Car is successfully created"));
+        }
+    }
+}
